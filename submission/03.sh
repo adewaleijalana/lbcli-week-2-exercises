2b0ce427893ff1ede19402de9281b3be6698b656
adewaleijalana <adewaleijalana@gmail.com>
2025-03-26 09:07:08 +0100

    fixing address creation

--- submission/03.sh
+++ submission/03.sh
@@ -1,5 +1,5 @@
 # Created a SegWit address.
-address=$(bitcoin-cli -regtest getnewaddress)
+address=$(bitcoin-cli -regtest getnewaddress SEGWIT_ADDR bech32)
 
 # Add funds to the address.
 bitcoin-cli -regtest generatetoaddress 101 $address
