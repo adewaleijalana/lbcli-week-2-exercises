ed631091695c7425b5fb124b1b19e3586c2f3ad2
adewaleijalana <adewaleijalana@gmail.com>
2025-03-26 09:45:23 +0100

    returning only address

--- submission/03.sh
+++ submission/03.sh
@@ -2,8 +2,7 @@
 address=$(bitcoin-cli -regtest getnewaddress SEGWIT_ADDR bech32)
 
 # Add funds to the address.
-bitcoin-cli -regtest generatetoaddress 101 $address
-sleep 1
+bitcoin-cli -regtest generatetoaddress 101 $address > /dev/null
 
 # Return only the Address
 echo "$address"
