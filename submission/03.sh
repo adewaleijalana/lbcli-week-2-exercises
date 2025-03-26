de41089185cdd0a2cd5df1725e84d8407a14be5d
adewaleijalana <adewaleijalana@gmail.com>
2025-03-26 09:03:58 +0100

    generate a SegWit adddress, add fund to the address and returned the address

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,8 @@
 # Created a SegWit address.
+address=$(bitcoin-cli -regtest getnewaddress)
+
 # Add funds to the address.
+bitcoin-cli -regtest generatetoaddress 101 $address
+
 # Return only the Address
+echo $address
